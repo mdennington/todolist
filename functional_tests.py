3712d04e08be438ef87606faee58b4fc8289c464
Mark Dennington <mdennington7@gmail.com>
2019-04-26 23:05:03 +0100

    First FT with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_later(self):
+        # Open Homepage
+        self.browser.get('http://localhost:8000')
+
+        # Check title is "To Do"
+        self.assertIn('To Do', self.browser.title)
+        self.fail('Finish The Test!')
+
+        # Enter a to do item
+
+        # Type "Read Chapter 6 of Two Scoops"
+
+        # Check page updates and lists the entry as
+        # 1. Read Chapter 6 of Two Scoops
+
+        # Add a second entry "Read Chapter 7 of Two Scoops"
+
+        # Check page update and lists the entries now as
+        # 1. Read Chapter 6 of Two Scoops
+        # 2. Read Chapter 7 of Two Scoops
+
+        # Check To Do List with Test Name Edith in URL
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
