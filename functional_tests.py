ce5262b1dafd2b7366b875f1952dbfd5a3af8453
Mark Dennington <mdennington7@gmail.com>
2019-04-27 12:02:46 +0100

    Added HTML tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,16 +17,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # Check title is "To Do"
         self.assertIn('To Do', self.browser.title)
-        self.fail('Finish The Test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To Do', header_text)
 
         # Enter a to do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Type "Read Chapter 6 of Two Scoops"
+        inputbox.send_keys('Read Chapter 6 of Two Scoops')
 
         # Check page updates and lists the entry as
         # 1. Read Chapter 6 of Two Scoops
+        inputbox.send_keys(Keys.Enter)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Read Chapter 6 of Two Scoops' for row in rows)
+        )
+
+    
 
         # Add a second entry "Read Chapter 7 of Two Scoops"
+        self.fail('Finish the Test')
 
         # Check page update and lists the entries now as
         # 1. Read Chapter 6 of Two Scoops
--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,25 @@
 # resolve URLS and find the view function they map to
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+# Create http object, pass to home page 
+# view and check html that returns
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request) 
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To Do Lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
+    
 
