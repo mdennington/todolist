73ff46edded9518d49f0df72956e3fbf02b2a6e4
Mark Dennington <mdennington7@gmail.com>
2019-04-28 21:49:17 +0100

    refactored time outs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,15 +15,22 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def send_input_to_list_item(self, inputbox, item_text):
         inputbox.send_keys(item_text)
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
     def test_can_start_a_list_and_retrieve_later(self):
         # Open Homepage
@@ -41,7 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Type "Read Chapter 6 of Two Scoops"
         self.send_input_to_list_item(inputbox,'Read Chapter 6 of Two Scoops')
 
-        self.check_for_row_in_list_table( "1: Read Chapter 6 of Two Scoops")
+        self.wait_for_row_in_list_table( "1: Read Chapter 6 of Two Scoops")
 
         # Add a second entry "Read Chapter 7 of Two Scoops"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -50,8 +60,8 @@ class NewVisitorTest(LiveServerTestCase):
         # Check page update and lists the entries now as
         # 1. Read Chapter 6 of Two Scoops
         # 2. Read Chapter 7 of Two Scoops
-        self.check_for_row_in_list_table("1: Read Chapter 6 of Two Scoops")
-        self.check_for_row_in_list_table("2: Read Chapter 7 of Two Scoops")
+        self.wait_for_row_in_list_table("1: Read Chapter 6 of Two Scoops")
+        self.wait_for_row_in_list_table("2: Read Chapter 7 of Two Scoops")
 
 
         # Check To Do List with Test Name Edith in URL
