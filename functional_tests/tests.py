0a144ae23e6018668c4826884ab6f6f62cada337
Mark Dennington <mdennington7@gmail.com>
2019-04-30 17:49:44 +0100

    URL includes added

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -96,6 +96,30 @@ class NewVisitorTest(LiveServerTestCase):
 
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Read Chapter 2 of Two Scoops', page_text)
+    
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and checks it is centre aligned
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
 
 
