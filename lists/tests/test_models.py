e595d1eb410b618822c094229eb90481417385b6
Mark Dennington <mdennington7@gmail.com>
2019-05-06 22:46:06 +0100

    Adjust List View to do Validation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,9 +1,9 @@
 # resolve is function used by Django to 
 # resolve URLS and find the view function they map to
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+
 from lists.models import Item, List
-
-
 
 class ListAndItemsModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -33,3 +33,9 @@ class ListAndItemsModelsTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
         
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
+
 
 from lists.models import Item, List
 # Create your views here.
@@ -13,11 +15,24 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'],list=list_ )
+    item = Item.objects.create(text=request.POST['item_text'],list=list_ )
+    item.full_clean()
     return redirect(f'/lists/{list_.id}/')
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+def new_list(request):
+    list_ = List.objects.create()
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
 
