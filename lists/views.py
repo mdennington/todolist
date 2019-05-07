28a526259721a7bf8b908372e88827bfe699e58f
Mark Dennington <mdennington7@gmail.com>
2019-05-07 22:34:18 +0100

    Removed hard-code URLs

--- lists/views.py
+++ lists/views.py
@@ -18,7 +18,7 @@ def view_list(request, list_id):
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect(f'/lists/{list_.id}/')
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
 
@@ -28,7 +28,7 @@ def new_list(request):
     list_ = List.objects.create()
     item = Item.objects.create(text=request.POST['item_text'],list=list_ )
     item.full_clean()
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(list_)
 
 def new_list(request):
     list_ = List.objects.create()
