b78ad65f73279d789713e58584251dc7647edd01
Mark Dennington <mdennington7@gmail.com>
2019-05-12 21:28:49 +0100

    fixed failing FT on Title

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -15,7 +15,7 @@ class NewVisitorTest(FunctionalTest):
         self.browser.get(self.live_server_url)
 
         # Check title is "To Do"
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn('To Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To Do', header_text)
 
