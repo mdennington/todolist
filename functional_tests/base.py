20b86e960633c37bfea24c49e748e25e8614b103
Mark Dennington <mdennington7@gmail.com>
2019-05-28 10:13:15 +0100

    Add fabric code for session testing

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,7 @@
 import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from .server_tools import reset_database
 from selenium.common.exceptions import WebDriverException
 import time
 
@@ -28,6 +29,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
