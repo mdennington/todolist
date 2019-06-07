60a7875d66ad984cb54b41ab3af7282bf2d6b740
Mark Dennington <mdennington7@gmail.com>
2019-06-07 13:13:20 +0100

    cleaned up gitignore

--- fabfile.py
+++ fabfile.py
@@ -1,37 +1,20 @@
-from invoke import task, Context
-from fabric import Connection, config
-
-import warnings
-
-warnings.filterwarnings(
-        action='ignore',
-        module=r'.*paramiko*'
-)
+from fabric.api import run, task, local
 
 
 @task
-def gitlog(c):
-    local_ctx = Context()
-    local_ctx.run('git log --oneline')
+def gitlog():
+    local('git log --oneline')
 
 
-@task
-def deploy(c):
-    local_ctx = Context()
-    local_ctx.run('pip freeze > requirements.txt')
-    local_ctx.run('git add .')
-    print("enter your git commit comment: ")
-    comment = input()
-    local_ctx.run('git commit -m "%s"' % comment)
-    local_ctx.run('git push -u origin master')
-    local_ctx.run('heroku maintenance:on')
-    local_ctx.run('git push heroku master')
-    local_ctx.run('heroku maintenance:off')
-
-
-@task
-def pushheroku(c):
-    local_ctx = Context()
-    local_ctx.run('heroku maintenance:on')
-    local_ctx.run('git push -f heroku master')
-    local_ctx.run('heroku maintenance:off')
+# @task
+# def deploy(c):
+#     run('pip freeze > requirements.txt')
+#     run('git add .')
+#     print("enter your git commit comment: ")
+#     comment = input()
+#     run('git commit -m "%s"' % comment)
+#     run('git push -u origin master')
+#     run('')
+#     with settings(host_string=f'mdennington@ssh.pythonanywhere.com'):
+#         with prefix('workon todolist-venv'):
+#             run(f'python --version')
